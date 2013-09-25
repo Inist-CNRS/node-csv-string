08bd33ecf3d40ec58751b02f91c9433a4d4a595a
Nicolas Thouvenin <nthouvenin@gmail.com>
2013-09-25 10:20:40 +0200

    work in progress

--- bench/fromStream.js
+++ bench/fromStream.js
@@ -4,7 +4,9 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 var buffer = '';
 process.stdin.on('data', function (chunk) {
-    buffer += chunk.toString();
+    //console.log('uuu', buffer.slice(-10), '|', chunk.toString().slice(0, 10))
+    buffer = buffer.concat('456}', chunk.toString());
+    //console.log('vvv', buffer.slice(0, 30));
     var r;
 
     r = CSV.readAll(buffer, ',', function (rows) {
@@ -15,7 +17,7 @@ process.stdin.on('data', function (chunk) {
       }
     )
     if (r < buffer.length) {
-      buffer = buffer.slice(r);
+      buffer = '{123' + buffer.slice(r) ;
     }
     else {
       buffer = '';
