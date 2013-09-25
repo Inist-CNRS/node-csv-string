71463f74b4d1e776ffe4d49ebb5ab5a4a7cfba4c
Nicolas Thouvenin <nthouvenin@gmail.com>
2013-09-25 12:07:40 +0200

    examples works

--- bench/fromStream.js
+++ bench/fromStream.js
@@ -1,27 +1,16 @@
 var CSV = require(__dirname + '/..')
 
+var stream = CSV.createStream();
+
+stream.on('data', function (rows) {
+    rows.forEach(function (item) {
+        process.stdout.write(CSV.stringify(item));
+      }
+    );
+  }
+)
+
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
-var buffer = '';
-process.stdin.on('data', function (chunk) {
-    //console.log('uuu', buffer.slice(-10), '|', chunk.toString().slice(0, 10))
-    buffer = buffer.concat('456}', chunk.toString());
-    //console.log('vvv', buffer.slice(0, 30));
-    var r;
+process.stdin.pipe(stream);
 
-    r = CSV.readAll(buffer, ',', function (rows) {
-        rows.forEach(function(item) {
-            process.stdout.write(CSV.stringify(item));
-          }
-        )
-      }
-    )
-    if (r < buffer.length) {
-      buffer = '{123' + buffer.slice(r) ;
-    }
-    else {
-      buffer = '';
-    }
-    
-  }
-);
--- bench/fromString.js
+++ bench/fromString.js
@@ -7,7 +7,7 @@ process.stdin.on('data', function (chunk) {
     buffer += chunk.toString();
   }
 );
-process.stdin.on('end', function (chunk) {
+process.stdin.on('end', function () {
     var r = CSV.readAll(buffer, ',', function (rows) {
         rows.forEach(function(item) {
             process.stdout.write(CSV.stringify(item));
